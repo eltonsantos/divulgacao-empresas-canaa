32e634387b744beb0c8d656e7ad769f3d3841110
Elton Santos <eltonaxl@hotmail.com>
2021-03-20 10:04:33 -0300

    Trying persist in localstorage

--- src/components/CadastroEmpresa/index.jsx
+++ src/components/CadastroEmpresa/index.jsx
@@ -2,36 +2,144 @@ import React, { useState } from 'react';
 
 function CadastroEmpresa() {
 
+  const [id, setId] = useState('');
   const [nome, setNome] = useState('');
-  const [empresa, setempresa] = useState('');
+  const [empresa, setEmpresa] = useState('');
   const [categoria, setCategoria] = useState('');
   const [endereco, setEndereco] = useState('');
   const [bairro, setBairro] = useState('');
   const [cep, setCep] = useState('');
   const [telefone, setTelefone] = useState('');
   const [descricao, setDescricao] = useState('');
+  const [error, setError] = useState('');
+  const [lista, setLista] = useState([]);
 
   const adicionar = (e) => {
+    e.preventDefault();
+
+    // Validações simples
+    if(!nome.trim()){
+      setError('Campo nome está vazio');
+    } else if(!empresa.trim()){
+      setError('Campo empresa está vazio');
+    } else if(!categoria.trim()){
+      setError('Campo categoria está vazio');
+    } else if(!cep.trim()){
+      setError('Campo cep está vazio');
+    } else if(!endereco.trim()){
+      setError('Campo endereco está vazio');
+    } else if(!bairro.trim()){
+      setError('Campo bairro está vazio');
+    } else if(!telefone.trim()){
+      setError('Campo telefone está vazio');
+    }
+
+    const list = {
+      nome,
+      empresa,
+      categoria,
+      cep,
+      endereco,
+      bairro,
+      telefone,
+      descricao
+    }
+
+    try {
+      const data = JSON.parse(localStorage.getItem(list));
+      
+      localStorage.setItem('lista', JSON.stringify(data));
+
+      console.log('cadastra');
+
+    } catch (e) {
+      console.log(e);
+      setError('Erro ao cadastrar');
+    }
     
   }
+
+  const apagar = (id) => {}
+
+  const editar = (id) => {}
 
   return (
     <div>
       <h1>Cadastrar Empresa</h1>
 
       <form onSubmit={adicionar} className="form-group">
-        <input className="form-control" type="text" placeholder="Nome do irmão" value={nome} />
-        <input className="form-control mt-2" type="text" placeholder="Nome da empresa" value={empresa} />
-        <input className="form-control mt-2" type="text" placeholder="Categoria" value={categoria} />
-        <input className="form-control mt-2" type="text" placeholder="Endereço" value={endereco} />
-        <input className="form-control mt-2" type="text" placeholder="Bairro" value={bairro} />
-        <input className="form-control mt-2" type="text" placeholder="CEP" value={cep} />
-        <input className="form-control mt-2" type="text" placeholder="Telefone" value={telefone} />
-        <textarea className="form-control mt-2" type="text" placeholder="Descrição" value={descricao}></textarea>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Nome do irmão"
+          value={nome}
+          onChange={(e) => {setNome(e.target.value)}}
+        />
+        <input
+          className="form-control mt-2"
+          type="text"
+          placeholder="Nome da empresa"
+          value={empresa}
+          onChange={(e) => {setEmpresa(e.target.value)}}
+        />
+        <input
+          className="form-control mt-2"
+          type="text"
+          placeholder="Categoria"
+          value={categoria}
+          onChange={(e) => {setCategoria(e.target.value)}}
+        />
+        <input
+          className="form-control mt-2"
+          type="text"
+          placeholder="CEP"
+          value={cep} onChange={(e) => {setCep(e.target.value)}}
+        />
+        <input
+          className="form-control mt-2"
+          type="text"
+          placeholder="Endereço"
+          value={endereco}
+          onChange={(e) => {setEndereco(e.target.value)}}
+        />
+        <input
+          className="form-control mt-2"
+          type="text"
+          placeholder="Bairro"
+          value={bairro}
+          onChange={(e) => {setBairro(e.target.value)}}
+        />
+        <input
+          className="form-control mt-2"
+          type="text"
+          placeholder="Telefone"
+          value={telefone}
+          onChange={(e) => {setTelefone(e.target.value)}}
+        />
+        <textarea
+          className="form-control mt-2"
+          type="text"
+          placeholder="Descrição"
+          value={descricao}
+          onChange={(e) => {setDescricao(e.target.value)}}
+        >
+        </textarea>
 
         <input className="btn btn-info btn-block mt-2" type="submit" />
-        
+
       </form>
+      {
+        error ?
+        (
+          <div className="alert alert-danger">
+            {error}
+          </div>
+        )
+        :
+        (
+          <span></span>
+        )
+      }
 
     </div>
   );
--- src/components/ListaEmpresas/index.jsx
+++ src/components/ListaEmpresas/index.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function ListaEmpresas() {
+
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+    const getLista = () => {
+            
+    }
+    getLista();
+  }, []);
+
   return (
-    <div>
+    <>  
       <h1>Lista de empresas</h1>
-    </div>
-  )
+      <div className="row">
+        <div className="col">
+          <ul  className="list-group">
+            {
+              lista.length !== 0 ?
+              (
+                <li className="list-group-item">
+                  <button className="btn btn-info float-right">Detalhes</button>
+                </li>
+              )
+              :
+              (
+                <span>Não há empresa cadastrada.</span>
+              )
+            }
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ListaEmpresas
